9e7bf509b546dec1eb35e471f58dc529d144f294
NaufalDekha002 <naufalgumboy@gmail.com>
2023-03-17 16:53:54 +0700

    tugas-heroku

--- controller/coba.go
+++ controller/coba.go
@@ -1,11 +1,18 @@
 package controller
 
 import (
+	"github.com/NaufalDekha002/dekha-ulbi/config"
 	"github.com/aiteung/musik"
+	cek "github.com/aiteung/presensi"
 	"github.com/gofiber/fiber/v2"
 )
 
 func Homepage(c *fiber.Ctx) error {
 	ipaddr := musik.GetIPaddress()
 	return c.JSON(ipaddr)
+}
+
+func GetPresensi(c *fiber.Ctx) error {
+	ps := cek.GetPresensiCurrentMonth(config.Ulbimongoconn)
+	return c.JSON(ps)
 }
