8c603354dd90c980e034d9ab79c5fe452788c2c1
NaufalDekha002 <naufalgumboy@gmail.com>
2023-04-12 14:43:08 +0700

    Push

--- url/url.go
+++ url/url.go
@@ -7,6 +7,17 @@ import (
 	"github.com/gofiber/websocket/v2"
 )
 
+// func Web(page *fiber.App) {
+// 	page.Post("/api/whatsauth/request", controller.PostWhatsAuthRequest)  //API from user whatsapp message from iteung gowa
+// 	page.Get("/ws/whatsauth/qr", websocket.New(controller.WsWhatsAuthQR)) //websocket whatsauth
+// 	page.Get("/", controller.Home)                                        //ujicoba panggil package musik
+// 	page.Get("/presensi", controller.GetPresensi)
+// 	page.Get("/peneliti", controller.GetPeneliti)
+// 	page.Get("/hasil teliti", controller.GetHasilTeliti)
+// 	page.Get("/all", controller.GetAll)
+// 	page.Get("/semua", controller.GetAllPresensi)
+// }
+
 func Web(page *fiber.App) {
 	page.Post("/api/whatsauth/request", controller.PostWhatsAuthRequest)  //API from user whatsapp message from iteung gowa
 	page.Get("/ws/whatsauth/qr", websocket.New(controller.WsWhatsAuthQR)) //websocket whatsauth
