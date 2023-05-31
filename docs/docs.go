ee26ec906870cf5400416bb47f392081de8ad5ca
NaufalDekha002 <naufalgumboy@gmail.com>
2023-05-31 15:04:25 +0700

    latihan11

--- docs/docs.go
+++ docs/docs.go
@@ -96,7 +96,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "penelitian-tugas.herokuapp.com",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Fiber Example API",
